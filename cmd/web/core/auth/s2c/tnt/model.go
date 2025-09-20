993c2f2812e82eb73734a8d65e483a0e37fe05bc
andrewah64 <andrewah64@gmail.com>
2025-09-20 22:34:20 +0100

    Hook in to procs/funcs

--- cmd/web/core/auth/s2c/tnt/model.go
+++ cmd/web/core/auth/s2c/tnt/model.go
@@ -16,6 +16,31 @@ import (
 	"github.com/andrewah64/base-app-client/internal/common/core/db"
 )
 
+func DelSpc (ctx *context.Context, logger *slog.Logger, conn *pgxpool.Conn, tntId int, spcId []int, exptErrs []string) error {
+	var (
+		sprocCall   = "call web_core_auth_s2c_tnt_mod.del_spc(@p_tnt_id, @p_spc_id)"
+		sprocParams = pgx.NamedArgs{
+			"p_tnt_id" : tntId,
+			"p_spc_id" : spcId,
+		}
+	)
+
+	sprocErr := db.Sproc(ctx, logger, conn, sprocCall, sprocParams, exptErrs)
+	if sprocErr != nil {
+		logger.LogAttrs(*ctx, slog.LevelDebug, "call sproc",
+			slog.String("sprocCall" , sprocCall),
+			slog.String("error"     , sprocErr.Error()),
+			slog.Int   ("tntId"     , tntId),
+			slog.Any   ("spcId"     , spcId),
+			slog.Any   ("exptErrs"  , exptErrs),
+		)
+
+		return sprocErr
+	}
+
+	return nil
+}
+
 type Opt struct {
 	Key   string
 	Id    int
@@ -170,6 +195,42 @@ func GetS2gInf (ctx *context.Context, logger *slog.Logger, conn *pgxpool.Conn, t
 	return rs, rErr
 }
 
+type SpcInf struct {
+	SpcId      int
+	SpcNm      string
+	SpcCnNm    string
+	SpcIncTs   time.Time
+	SpcExpTs   time.Time
+	SpcEnabled bool
+}
+
+func GetSpcInf (ctx *context.Context, logger *slog.Logger, conn *pgxpool.Conn, tntId int, spcNm string, spcIncTs *time.Time, spcExpTs *time.Time, spcEnabled *bool, offset int, limit int) ([]SpcInf, error) {
+	rs, rErr := db.DataSet[SpcInf](ctx, logger, conn,
+		func(ctx *context.Context, tx *pgx.Tx)(string, string, *pgx.Rows, error){
+			dbFunc := "spc_inf"
+			qry    := fmt.Sprintf("select web_core_auth_s2c_tnt_inf.%v($1, $2, $3, $4, $5, $6, $7, $8)", dbFunc)
+
+			c, cErr := (*tx).Query(*ctx, qry, dbFunc, tntId, spcNm, spcIncTs, spcExpTs, spcEnabled, offset, limit)
+			if cErr != nil {
+				slog.LogAttrs(*ctx, slog.LevelError, "get dataset",
+					slog.String("error"      , cErr.Error()),
+					slog.String("qry"        , qry),
+					slog.Int   ("tntId"      , tntId),
+					slog.String("spcNm"      , spcNm),
+					slog.Any   ("spcIncTs"   , spcIncTs),
+					slog.Any   ("spcExpTs"   , spcExpTs),
+					slog.Any   ("spcEnabled" , spcEnabled),
+				)
+
+				return qry, dbFunc, nil, fmt.Errorf("call database function: %w", cErr)
+			}
+
+			return qry, dbFunc, &c, nil
+		})
+
+	return rs, rErr
+}
+
 func PatchS2c (ctx *context.Context, logger *slog.Logger, conn *pgxpool.Conn, tntId int, s2cEnabled bool, s2cEntityId string, aumId int, by string, uts time.Time, exptErrs []string) error {
 	var (
 		sprocCall   = "call web_core_auth_s2c_tnt_mod.mod_s2c(@p_tnt_id, @p_s2c_enabled, @p_s2c_entity_id, @p_aum_id, @p_by, @p_uts)"
@@ -225,6 +286,41 @@ func PatchS2g (ctx *context.Context, logger *slog.Logger, conn *pgxpool.Conn, tn
 			slog.String("s2gCrtOrg" , s2gCrtOrg),
 			slog.String("by"        , by),
 			slog.Any   ("uts"       , uts),
+			slog.Any   ("exptErrs"  , exptErrs),
+		)
+
+		return sprocErr
+	}
+
+	return nil
+}
+
+func PostSpc (ctx *context.Context, logger *slog.Logger, conn *pgxpool.Conn, tntId int, spcCnNm string, spcOrgNm string, spcEncCrt []byte, spcEncPvk []byte, spcSgnCrt []byte, spcSgnPvk []byte, spcIncTs time.Time, spcExpTs time.Time, by string, exptErrs []string) error {
+	var (
+		sprocCall   = "call web_core_auth_s2c_tnt_mod.reg_spc(@p_tnt_id, @p_spc_cn_nm, @p_spc_org_nm, @p_spc_enc_crt, @p_spc_enc_pvk, @p_spc_sgn_crt, @p_spc_sgn_pvk, @p_spc_inc_ts, @p_spc_exp_ts, @p_by)"
+		sprocParams = pgx.NamedArgs{
+			"p_tnt_id"      : tntId,
+			"p_spc_cn_nm"   : spcCnNm,
+			"p_spc_org_nm"  : spcOrgNm,
+			"p_spc_enc_crt" : spcEncCrt,
+			"p_spc_enc_pvk" : spcEncPvk,
+			"p_spc_sgn_crt" : spcSgnCrt,
+			"p_spc_sgn_pvk" : spcSgnPvk,
+			"p_spc_inc_ts"  : spcIncTs,
+			"p_spc_exp_ts"  : spcExpTs,
+			"p_by"          : by,
+		}
+	)
+
+	sprocErr := db.Sproc(ctx, logger, conn, sprocCall, sprocParams, exptErrs)
+	if sprocErr != nil {
+		logger.LogAttrs(*ctx, slog.LevelDebug, "call sproc",
+			slog.String("sprocCall" , sprocCall),
+			slog.String("error"     , sprocErr.Error()),
+			slog.Int   ("tntId"     , tntId),
+			slog.String("spcCnNm"   , spcCnNm),
+			slog.String("spcOrgNm"  , spcOrgNm),
+			slog.String("by"        , by),
 			slog.Any   ("exptErrs"  , exptErrs),
 		)
 
